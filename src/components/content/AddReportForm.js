df387fe5fc892417ab6c8e85c64eedd30447dd47
Bruno Souza <brunoezully@gmail.com>
2022-09-14 13:39:58 -0300

    Pequenas mudanças

--- src/components/content/AddReportForm.js
+++ src/components/content/AddReportForm.js
@@ -61,7 +61,21 @@ function AddReportForm(props) {
              <div className="add-report-form-controls" id="report">
                 <div className="add-report-form-control" >
                     <label>Mês</label>
-                    <input value={enteredMonth} onChange={onMonth} type="text" required/>
+                    {/* <input value={enteredMonth} onChange={onMonth} type="text" required/> */}
+                    <select onChange={onMonth}>
+                        <option value="Janeiro">Janeiro</option>
+                        <option value="Fevereiro">Fevereiro</option>
+                        <option value="Março">Março</option>
+                        <option value="Abril">Abril</option>
+                        <option value="Maio">Maio</option>
+                        <option value="Junho">Junho</option>
+                        <option value="Julho">Julho</option>
+                        <option value="Agosto">Agosto</option>
+                        <option value="Setembro">Setembro</option>
+                        <option value="Outubro">Outubro</option>
+                        <option value="Novembro">Novembro</option>
+                        <option value="Dezembro">Dezembro</option>
+                    </select>
                 </div>
                 <div className="add-report-form-control">
                     <label>Horas trabalhadas</label>

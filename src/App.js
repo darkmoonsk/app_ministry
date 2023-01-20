c7feddbc1c545842ca671e049663da3c0b32c476
Bruno Souza <brunoezully@gmail.com>
2023-01-20 12:45:52 -0300

    Iniciando implementacao do React Router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import classes from './App.module.css';
 import Context from './components/body/Context';
 import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 
 
@@ -11,8 +14,13 @@ function App() {
   return (
     <div className={classes["main-container"]}>
       <div className={classes.container}>
-          <Context.Provider value={[newReportAction, setNewReportAction]}>               
-            <Dashboard />
+          <Context.Provider value={[newReportAction, setNewReportAction]}>   
+            <BrowserRouter>  
+              <Routes>
+                <Route path="/login" element={<Login />} />          
+                <Route path="/dashboard" element={<Dashboard />} />
+              </Routes>
+            </BrowserRouter>
           </Context.Provider>
       </div> 
     </div>

09e74486c589f8ac2191867a2e2a9f3e63799851
Bruno Souza <brunoezully@gmail.com>
2023-09-23 01:02:00 -0300

    refactor: add mais opções de anos O objetivo é igualar os anos do filtro com os do relatório

--- src/components/dashboard/DashboardFilters.js
+++ src/components/dashboard/DashboardFilters.js
@@ -16,6 +16,13 @@ export default function DashboardFilters() {
                 <option value="all">Todos</option>
                 <option value="2022">2022</option>
                 <option value="2023">2023</option>
+                <option value="2024">2024</option>
+                <option value="2025">2025</option>
+                <option value="2026">2026</option>
+                <option value="2027">2027</option>
+                <option value="2028">2028</option>
+                <option value="2029">2029</option>
+                <option value="2030">2030</option>
             </select>
         </div>
     );
